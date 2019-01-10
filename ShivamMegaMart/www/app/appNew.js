30fcc177be158ade84c4f44a6d2ca58de7e8b527
Ajay Paliwal <ajaypaliwal.cto@outlook.com>
2019-01-10 19:18:40 +0530

    Update appNew.js

--- ShivamMegaMart/www/app/appNew.js
+++ ShivamMegaMart/www/app/appNew.js
@@ -7,7 +7,7 @@ app.config(function ($routeProvider) {
 
     $routeProvider.when("/home", {
         controller: "homeController",
-        templateUrl: "/app/views/homeNew.html"
+        templateUrl: "app/views/homeNew.html"
     });
 
     $routeProvider.when("/login", {
